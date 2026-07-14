60f00bc024095033a4e13b6c9801da3f2740861b
CGSchaefer-hub <C.schaefer@mail.siebold-gymnasium.de>
2026-07-14 22:03:37 +0200

    Revise plugin details and include autoload
    Updated plugin description and added autoload requirement.

--- wp-user-excel-import.php
+++ wp-user-excel-import.php
@@ -1,11 +1,8 @@
 <?php
 /*
 Plugin Name: Benutzerimport aus Excel
-Plugin URI: https://example.de
-Description: Importiert Benutzer aus einer Excel-Datei.
+Description: Importiert Benutzer aus Excel-Dateien.
 Version: 1.0
-Author: Christian Schäfer
-License: GPL2
 */
 
 if (!defined('ABSPATH')) {
@@ -15,6 +12,7 @@ if (!defined('ABSPATH')) {
 define('UEI_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('UEI_PLUGIN_URL', plugin_dir_url(__FILE__));
 
+require_once UEI_PLUGIN_DIR . 'vendor/autoload.php';
 require_once UEI_PLUGIN_DIR . 'includes/class-plugin.php';
 
 UEI_Plugin::instance();
