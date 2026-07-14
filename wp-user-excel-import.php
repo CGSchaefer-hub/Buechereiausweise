82bb456491cbfdc599a22c471fe1a11d1f33df75
CGSchaefer-hub <C.schaefer@mail.siebold-gymnasium.de>
2026-07-14 22:06:17 +0200

    Implement user import functionality from Excel file
    Add handle_import method to process Excel file uploads and create users.

--- wp-user-excel-import.php
+++ wp-user-excel-import.php
@@ -16,3 +16,38 @@ require_once UEI_PLUGIN_DIR . 'vendor/autoload.php';
 require_once UEI_PLUGIN_DIR . 'includes/class-plugin.php';
 
 UEI_Plugin::instance();
+
+public function handle_import()
+{
+
+    if (!isset($_POST['_wpnonce']))
+        return;
+
+    if (!wp_verify_nonce($_POST['_wpnonce'], 'uei_import'))
+        return;
+
+    if (empty($_FILES['excel']['tmp_name']))
+        return;
+
+    $rows = UEI_Excel::load($_FILES['excel']['tmp_name']);
+
+    if (count($rows) < 2)
+        return;
+
+    $header = array_shift($rows);
+
+    foreach ($rows as $row)
+    {
+
+        $user=[];
+
+        foreach($header as $index=>$field)
+        {
+            $user[$field]=$row[$index] ?? '';
+        }
+
+        UEI_User::create($user);
+
+    }
+
+}
