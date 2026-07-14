84ac8164f99aeba4c44410da618076a3293e9ab9
CGSchaefer-hub <C.schaefer@mail.siebold-gymnasium.de>
2026-07-14 21:59:12 +0200

    Add UEI_Plugin class for plugin initialization

---
+++ includes/class-plugin.php
@@ -0,0 +1,34 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class UEI_Plugin
+{
+
+    private static $instance = null;
+
+    public static function instance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    private function __construct()
+    {
+
+        require_once UEI_PLUGIN_DIR . 'class_exists';
+
+        require_once UEI_PLUGIN_DIR . 'includes/class-admin.php';
+        require_once UEI_PLUGIN_DIR . 'includes/class-importer.php';
+        require_once UEI_PLUGIN_DIR . 'includes/class-user.php';
+        require_once UEI_PLUGIN_DIR . 'includes/class-excel.php';
+
+        new UEI_Admin();
+        new UEI_Importer();
+    }
+}
