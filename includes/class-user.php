9681df9fe7f2ac57de884a0c1530743bbefdda01
CGSchaefer-hub <C.schaefer@mail.siebold-gymnasium.de>
2026-07-14 22:07:16 +0200

    Refactor user creation logic in UEI_User class

--- includes/class-user.php
+++ includes/class-user.php
@@ -7,33 +7,44 @@ if (!defined('ABSPATH')) {
 class UEI_User
 {
 
-    public static function create($data)
+public static function create($data)
+{
+
+    if(empty($data['Benutzername']))
+        return;
+
+    if(username_exists($data['Benutzername']))
+        return;
+
+    $password=wp_generate_password();
+
+    $email='';
+
+    if(!empty($data['E-Mail']))
+        $email=$data['E-Mail'];
+
+    $id=wp_insert_user([
+
+        'user_login'=>$data['Benutzername'],
+        'user_pass'=>$password,
+        'user_email'=>$email,
+        'first_name'=>$data['Vorname'] ?? '',
+        'last_name'=>$data['Nachname'] ?? '',
+        'role'=>'subscriber'
+
+    ]);
+
+    if(is_wp_error($id))
+        return;
+
+    foreach($data as $key=>$value)
     {
 
-        $password = wp_generate_password(12);
-
-        $user = [
-            'user_login' => $data['Benutzername'],
-            'user_pass' => $password,
-            'user_email' => $data['E-Mail'],
-            'first_name' => $data['Vorname'],
-            'last_name' => $data['Nachname'],
-            'role' => 'subscriber'
-        ];
-
-        $id = wp_insert_user($user);
-
-        if (is_wp_error($id)) {
-            return $id;
-        }
-
-        foreach ($data as $key => $value) {
-
-            update_user_meta($id, sanitize_key($key), $value);
-
-        }
-
-        return $id;
+        update_user_meta(
+            $id,
+            sanitize_key($key),
+            $value
+        );
 
     }
 
